555448bfd035379c180c31e7bb6311b2aa08df10
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-09 15:35:39 -0300

    Proyecto iniciado.

---
+++ src/main/java/com/hotelsystem/HotelsystemApplication.java
@@ -0,0 +1,13 @@
+package com.hotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotelsystem/HotelsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
