11d03d3b30d333390a998b0d983f9a0f65eee4c1
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-20 16:22:50 -0300

    room controller.

--- src/main/java/com/hotelsystem/controllers/RoomController.java
+++ src/main/java/com/hotelsystem/controllers/RoomController.java
@@ -4,14 +4,43 @@ import com.hotelsystem.models.Room;
 import com.hotelsystem.services.api.RoomServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 public class RoomController {
     @Autowired
     private RoomServiceAPI roomServiceAPI;
 
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("list", roomServiceAPI.getAll());
 
+        return "index";
+    }
+
+    @GetMapping("/save/{id}")
+    public String showSave(@PathVariable("id") Long id, Model model){
+        if(id != null && id != 0) {
+            model.addAttribute("room", roomServiceAPI.get(id));
+        } else {
+            model.addAttribute("room", new Room());
+        }
+
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(Room room, Model model) {
+        roomServiceAPI.save(room);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@PathVariable Long id, Model model) {
+        roomServiceAPI.delete(id);
+
+        return "redirect:/";
+    }
 }
