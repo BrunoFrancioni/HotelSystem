223cb66886ebe65627ee8045c462c44631c1044b
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-11-25 17:38:47 -0300

    Utils

---
+++ src/main/java/com/hotelsystem/utils/DateParser.java
@@ -0,0 +1,13 @@
+package com.hotelsystem.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParser {
+    public static Date parseDate(String date) throws ParseException {
+        Date new_date = new SimpleDateFormat("yyyy/MM/dd").parse(date);
+
+        return new_date;
+    }
+}
