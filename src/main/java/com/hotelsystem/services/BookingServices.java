f10579f2607598d2f1867968b5ea91413bfffe0b
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-12-07 16:07:02 -0300

    Removing unnecesary imports.

--- src/main/java/com/hotelsystem/services/BookingServices.java
+++ src/main/java/com/hotelsystem/services/BookingServices.java
@@ -1,16 +1,17 @@
 package com.hotelsystem.services;
 
+import com.hotelsystem.models.Booking;
 import com.hotelsystem.models.Room;
 import com.hotelsystem.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
 @Service
+@Transactional
 public class BookingServices {
     @Autowired
     private BookingRepository bookingRepository;
