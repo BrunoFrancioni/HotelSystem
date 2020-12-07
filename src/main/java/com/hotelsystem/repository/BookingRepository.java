1fc6a5988563ae275c54ddb18d871f511f427111
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-12-07 16:04:55 -0300

    Nothing different.

--- src/main/java/com/hotelsystem/repository/BookingRepository.java
+++ src/main/java/com/hotelsystem/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import com.hotelsystem.models.Booking;
 import com.hotelsystem.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface BookingRepository extends JpaRepository<Booking,Long> {
+public interface BookingRepository extends JpaRepository<Booking,Long>, PagingAndSortingRepository<Booking,Long> {
 
     @Query("SELECT r FROM Room r WHERE r.availability = true AND NOT EXISTS " +
             "(SELECT b FROM Booking b WHERE (r.occupancy < :guests) OR (r.id_room = b.room.id_room AND "+
@@ -19,5 +20,4 @@ public interface BookingRepository extends JpaRepository<Booking,Long> {
             "(:to BETWEEN b.check_in AND b.check_out) OR " +
             "(:from < b.check_in AND :to > b.check_out)))) ORDER BY r.occupancy,r.price")
     List<Room> checkAvailableRooms(@Param("from") Date fromDate,@Param("to") Date toDate, @Param("guests") Integer guestsInteger);
-
 }
--- src/main/java/com/hotelsystem/repository/UserRepository.java
+++ src/main/java/com/hotelsystem/repository/UserRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    public Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     @Modifying
     @Transactional
