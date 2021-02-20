0b84d787e7216e6eb725a6d609e71bca4b4c25a3
VictorBoscoscuro <victor.boscoscuro1@gmail.com>
2021-02-20 18:08:03 -0300

    Booking save andando por fin

--- src/main/java/com/hotelsystem/repository/BookingRepository.java
+++ src/main/java/com/hotelsystem/repository/BookingRepository.java
@@ -20,14 +20,14 @@ public interface BookingRepository extends JpaRepository<Booking,Long> {
             "(SELECT b FROM Booking b WHERE (r.occupancy < :guests) OR (r.id_room = b.room.id_room AND "+
             "((:from BETWEEN b.check_in AND b.check_out) OR "+
             "(:to BETWEEN b.check_in AND b.check_out) OR " +
-            "(:from < b.check_in AND :to > b.check_out)))) ORDER BY r.occupancy,r.price")
+            "(:from <= b.check_in AND :to >= b.check_out)))) ORDER BY r.occupancy,r.price")
     List<Room> checkAvailableRooms(@Param("from") Date fromDate, @Param("to") Date toDate, @Param("guests") Integer guestsInteger);
 
     @Query("SELECT r FROM Room r WHERE r.availability = true AND NOT EXISTS " +
             "(SELECT b FROM Booking b WHERE (r.occupancy < :guests) OR (r.id_room = b.room.id_room AND "+
             "((:from BETWEEN b.check_in AND b.check_out) OR "+
             "(:to BETWEEN b.check_in AND b.check_out) OR " +
-            "(:from < b.check_in AND :to > b.check_out)))) ORDER BY r.occupancy,r.price")
+            "(:from <= b.check_in AND :to >= b.check_out)))) ORDER BY r.occupancy,r.price")
     Page<Room> checkAvailableRoomsPageable(Pageable pageable, @Param("from") Date fromDate, @Param("to") Date toDate, @Param("guests") Integer guestsInteger);
 
     @Query("SELECT b FROM Booking b WHERE b.room.id_room = :id_room AND " +
